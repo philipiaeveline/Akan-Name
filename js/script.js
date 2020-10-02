1acbb7bbad1b7d095009dbf76dbb59186b4d2b62
philipiaeveline <philipiaeveline@gmail.com>
2020-10-02 15:41:27 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function day (cc,yy,mm,dd){
     return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
   }
-  function () {
+  function  gd() {
   var cc = (document.getElementById("Id1").value);
   var yy = (document.getElementById("Id2").value);
   var mm = (document.getElementById("Id3").value);
@@ -15,8 +15,8 @@ function day (cc,yy,mm,dd){
     }
   var cc = parseInt(prompt("Enter Year"));
   var yy = parseInt(prompt("Months"));
-  var mm = parseInt(prompt("Days"));
-  var dd = parseInt(prompt("Enter the date of birth`<<<<<<<<"));
+  var mm = parseInt(prompt("days"));
+  var dd = parseInt(prompt("Enter the date of birth"));
   var gender= prompt("your gender")
   var result=day(cc,yy,mm,dd).toFixed();
   var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
